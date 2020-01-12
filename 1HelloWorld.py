b15b8c7e29e6862943036619ae865d5e24e903ca
Tarush Sinha <tarush.sinha@gmail.com>
2020-01-11 16:06:02 -0800

    First commit

---
+++ 1HelloWorld.py
@@ -0,0 +1,3 @@
+#Python is a high - level programming language
+##More on low - mid - high level langs here: https://stackoverflow.com/q/3468068/
+##tl;dr: Low Level Language: Little to no Abstraction from Computer's Instruction Set Architecture.
---
+++ prints.py
@@ -0,0 +1,20 @@
+# Prints can use '' or ""
+print('Print Examples:')
+# \n, \t, etc. can be used within quotes
+print("Format Print:\n\t\t\tcan do this!\n")
+
+# {} represent Sets, cannot store duplicates. While len(mood) should be 7, it is 6
+# more on Sets (basic uses) - membership testing, eliminating duplicate elements
+mood = {"sunny", 43, "rainy", "gloomy", "cloudy", "beautiful", "sunny"}
+print(f'The type of mood is {type(mood)}.')
+print(f'The type of mood is {len(mood)}.')
+
+# [] represent lists
+moodList = ["sunny", 43, "rainy", "gloomy", "cloudy", "beautiful"]
+print(f'The type of moodList is {type(moodList)}.')
+
+print('Printing the list:')
+# unnecessary to iterate through len(list), similar to enhanced for-loop in Java
+for i in moodList:
+    print(f'This is a {i} day')
+
